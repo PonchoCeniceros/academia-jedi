70bb6a1f0607f9e7ec635d19c2a2746a893c3239
ponchoceniceros <ponchoceniceros@outlook.com>
2026-05-04 15:45:23 -0600

    [rust] implementar mecanismo de katas para Rust

---
+++ katas/rust/contains_duplicate/src/main.rs
@@ -0,0 +1,23 @@
+struct Solution;
+
+impl Solution {
+    pub fn solve() {
+        // Implementa tu solución aquí
+        todo!()
+    }
+}
+
+fn main() {
+    // Solution::solve();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_contains_duplicate() {
+        // assert_eq!(Solution::solve(), expected);
+        todo!()
+    }
+}
---
+++ katas/rust/two_sum/src/main.rs
@@ -0,0 +1,47 @@
+use std::collections::HashMap;
+
+struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut hash_table: HashMap<i32, i32> = HashMap::new();
+        let mut x: i32 = 0;
+        let mut y: i32 = 0;
+
+        for (idx, val) in nums.iter().enumerate() {
+            y = target - val;
+
+            if let Some(&ans) = hash_table.get(&y) {
+                x = ans;
+                y = idx as i32;
+                return vec![x, y];
+            }
+            // es quivalente a:
+            //
+            // let ans = hash_table.get(&y);
+            // if ans != None {
+            //     x = *ans.unwrap();
+            //     y = idx as i32;
+            //     return vec![x, y];
+            // }
+            //
+            hash_table.insert(*val, idx as i32);
+        }
+        vec![x, y]
+    }
+}
+
+fn main() {
+    // Solution::solve();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_two_sum() {
+        assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+        assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+    }
+}
