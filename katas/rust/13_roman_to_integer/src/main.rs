111e1071c9bc9a7b960db370e96f82ea2bdee3b2
ponchoceniceros <ponchoceniceros@outlook.com>
2026-05-05 16:44:59 -0600

    [217] nuevos problemas

---
+++ katas/rust/13_roman_to_integer/src/main.rs
@@ -0,0 +1,104 @@
+struct Solution;
+// use std::collections::HashMap;
+
+/**
+ * Implementa tu solución aquí
+ *
+ */
+impl Solution {
+    pub fn roman_to_int(s: String) -> i32 {
+        println!("{}", s);
+        0
+    }
+}
+
+/**
+ * Pruebas unitarias
+ *
+ */
+fn main() {
+    let _ans = Solution::roman_to_int("X".to_string());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_roman_to_integer() {
+        assert_eq!(Solution::roman_to_int("X".to_string()), 0);
+        todo!()
+    }
+}
+
+/*
+class Solution:
+    table = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+        "IX": 9,
+        "XC": 90,
+        "CM": 900,
+        "IV": 4,
+        "XL": 40,
+        "CD": 400,
+    }
+
+    def romanToInt(self, s: str) -> int:
+        ans = 0
+        i = 0
+
+        while i < len(s):
+            if s[i] == "I":
+                if i + 1 < len(s) and s[i + 1] == "V":
+                    # Log.blue(f"{self.table['IV']}")
+                    ans = ans + self.table["IV"]
+                    i = i + 2
+                elif i + 1 < len(s) and s[i + 1] == "X":
+                    # Log.blue(f"{self.table['IX']}")
+                    ans = ans + self.table["IX"]
+                    i = i + 2
+                else:
+                    # Log.blue(f"{self.table[s[i]]}")
+                    ans = ans + self.table[s[i]]
+                    i = i + 1
+
+            elif s[i] == "X":
+                if i + 1 < len(s) and s[i + 1] == "L":
+                    # Log.blue(f"{self.table['XL']}")
+                    ans = ans + self.table["XL"]
+                    i = i + 2
+                elif i + 1 < len(s) and s[i + 1] == "C":
+                    # Log.blue(f"{self.table['XC']}")
+                    ans = ans + self.table["XC"]
+                    i = i + 2
+                else:
+                    # Log.blue(f"{self.table[s[i]]}")
+                    ans = ans + self.table[s[i]]
+                    i = i + 1
+
+            elif s[i] == "C":
+                if i + 1 < len(s) and s[i + 1] == "D":
+                    # Log.blue(f"{self.table['CD']}")
+                    ans = ans + self.table["CD"]
+                    i = i + 2
+                elif i + 1 < len(s) and s[i + 1] == "M":
+                    # Log.blue(f"{self.table['CM']}")
+                    ans = ans + self.table["CM"]
+                    i = i + 2
+                else:
+                    # Log.blue(f"{self.table[s[i]]}")
+                    ans = ans + self.table[s[i]]
+                    i = i + 1
+
+            else:
+                # Log.blue(f"{self.table[s[i]]}")
+                ans = ans + self.table[s[i]]
+                i = i + 1
+        return ans
+*/
