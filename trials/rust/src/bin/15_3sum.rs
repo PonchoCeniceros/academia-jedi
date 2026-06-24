7a8aabf6eb562c6be04dfae18659b4cc15e20dc9
Giovanny Alfonso Chávez Ceniceros <gio.icc.uach@gmail.com>
2026-06-23 20:26:12 -0600

    [main] problema 15 en proceso - WA 105 / 316 testcases passed

--- trials/rust/src/bin/15_3sum.rs
+++ trials/rust/src/bin/15_3sum.rs
@@ -1,4 +1,5 @@
 use colored::*;
+use std::collections::HashSet;
 
 struct Solution;
 
@@ -7,43 +8,51 @@ struct Solution;
  *
  */
 impl Solution {
-    /**
-     *
-     */
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        for k in 0..nums.len() {
-            // @todo condicionar los valores de los indices segun el valor de k
-            let (mut i, mut j) = (0_usize, nums.len() - 1);
-            let x = nums[k];
+        let mut set: HashSet<Vec<i32>> = HashSet::new();
+        let mut arr = nums.clone();
+        arr.sort_by(|a, b| b.cmp(a));
+
+        for k in 0..arr.len() {
+            let mut i: usize = if k == 0 { 1 } else { 0 };
+            let mut j: usize = if k == arr.len() - 1 {
+                arr.len() - 2
+            } else {
+                arr.len() - 1
+            };
 
             while j > i {
-                let (y, z) = (nums[i], nums[j]);
-                let s = y + z;
-                if s == -x {
-                    println!("(x,y,z) = ({},{},{})", x, y, z);
+                let (x, y, z) = (arr[k], arr[i], arr[j]);
+                let s = x + y + z;
+                if s == 0 {
+                    let mut ans = vec![x, y, z];
+                    ans.sort();
+                    set.insert(ans);
+                    // println!("(x,y,z) = ({},{},{})", x, y, z);
                     break;
                 }
 
-                if s > -x {
-                    j = if j == k {
-                        (j as i32 - 2) as usize
+                if s < 0 {
+                    j = if j.saturating_sub(1) == k {
+                        j.saturating_sub(2) // evitando el casteo (j as i32 - 2) as usize
                     } else {
-                        (j as i32 - 1) as usize
+                        j.saturating_sub(1)
                     };
                 }
 
-                if s < -x {
-                    i = if i == k { i + 2 } else { i + 1 };
+                if s > 0 {
+                    i = if i + 1 == k { i + 2 } else { i + 1 };
                 }
             }
         }
 
-        vec![vec![0]]
+        let mut ans: Vec<Vec<i32>> = set.into_iter().collect();
+        ans.sort();
+        ans
     }
 }
 
 fn main() {
-    // let nums = vec![-1, 0, 1, 2, -1, -4];
     let nums = vec![-1, 0, 1, 2, -1, -4];
     let ans = Solution::three_sum(nums);
     println!("{}", format!("{:?}", ans).green().italic().underline());
@@ -55,12 +64,23 @@ mod tests {
 
     #[test]
     fn test_three_sum() {
-        let cases: [(Vec<i32>, Vec<Vec<i32>>); 3] = [
+        let cases: [(Vec<i32>, Vec<Vec<i32>>); 4] = [
             (vec![0, 1, 1], vec![]),
             (vec![0, 0, 0], vec![vec![0, 0, 0]]),
             (
                 vec![-1, 0, 1, 2, -1, -4],
                 vec![vec![-1, -1, 2], vec![-1, 0, 1]],
+            ),
+            (
+                vec![2, -3, 0, -2, -5, -5, -4, 1, 2, -2, 2, 0, 2, -4, 5, 5, -10],
+                vec![
+                    vec![-10, 5, 5],
+                    vec![-5, 0, 5],
+                    vec![-4, 2, 2],
+                    vec![-3, -2, 5],
+                    vec![-3, 1, 2],
+                    vec![-2, 0, 2],
+                ],
             ),
         ];
 
@@ -74,21 +94,3 @@ mod tests {
         }
     }
 }
-
-/*
-if sigma_0:
-   if sigma_3:
-       delta_1(i, j, j+2)
-   else:
-       delta_1(i, j, j+1)
-
-if sigma_1:
-    continue
-
-if sigma_2:
-       delta_0(i, j, j+1, j-2)
-
-if sigma_3:
-       delta_0(i, j, j+2, j-1)
-
-*/
