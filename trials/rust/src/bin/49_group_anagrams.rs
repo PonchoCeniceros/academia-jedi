4307f371a87756321cc98c7341153922df969592
Giovanny Alfonso Chávez Ceniceros <70242956+PonchoCeniceros@users.noreply.github.com>
2026-06-24 15:43:56 -0600

    [main] problema 49 solucionado

--- trials/rust/src/bin/49_group_anagrams.rs
+++ trials/rust/src/bin/49_group_anagrams.rs
@@ -1,5 +1,7 @@
 use colored::*;
 use katas::s;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry::{Occupied, Vacant};
 
 struct Solution;
 
@@ -8,8 +10,37 @@ struct Solution;
  *
  */
 impl Solution {
+    /**
+     * ordenar lexicograficamente un string dado
+     */
+    fn sort_string(src: String) -> String {
+        let mut aux: Vec<char> = src.chars().collect();
+        aux.sort();
+        aux.into_iter().collect()
+    }
+
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        vec![vec!["".to_string()]]
+        let mut set: HashMap<String, Vec<String>> = HashMap::new();
+        let mut ans: Vec<Vec<String>> = Vec::new();
+
+        for s in strs.into_iter() {
+            let k = Solution::sort_string(s.clone());
+
+            match set.entry(k) {
+                Vacant(e) => {
+                    e.insert(vec![s]);
+                }
+                Occupied(mut e) => {
+                    e.get_mut().push(s);
+                }
+            }
+        }
+
+        for c in set.values() {
+            ans.push(c.clone());
+        }
+
+        ans
     }
 }
 
@@ -32,24 +63,21 @@ mod tests {
 
     #[test]
     fn test_group_anagrams() {
-        let cases = [
-            (
-                vec![
-                    s!("eat"),
-                    s!("tea"),
-                    s!("tan"),
-                    s!("ate"),
-                    s!("nat"),
-                    s!("bat"),
-                ],
-                vec![
-                    vec![s!("bat")],
-                    vec![s!("nat"), s!("tan")],
-                    vec![s!("ate"), s!("eat"), s!("tea")],
-                ],
-            ),
-            // (, ),
-        ];
+        let cases = [(
+            vec![
+                s!("eat"),
+                s!("tea"),
+                s!("tan"),
+                s!("ate"),
+                s!("nat"),
+                s!("bat"),
+            ],
+            vec![
+                vec![s!("bat")],
+                vec![s!("nat"), s!("tan")],
+                vec![s!("ate"), s!("eat"), s!("tea")],
+            ],
+        )];
 
         for (input, expected) in cases {
             assert_eq!(
