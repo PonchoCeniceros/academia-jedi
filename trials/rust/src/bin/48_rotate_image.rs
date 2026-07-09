e3f298d36a76d39d050c237a65fd85f79b0c940a
Giovanny Alfonso Chávez Ceniceros <70242956+PonchoCeniceros@users.noreply.github.com>
2026-07-09 14:00:59 -0600

    [main] problema 48 en proceso

--- trials/rust/src/bin/48_rotate_image.rs
+++ trials/rust/src/bin/48_rotate_image.rs
@@ -12,20 +12,20 @@ impl Solution {
             return;
         }
 
-        for p in s..(s + n - 1) {
-            let (mut i, mut j) = (s, p);
+        for p in 0..(n - 1) {
+            let (mut i, mut j) = (0, p);
             let (mut l, mut m) = (0_usize, 0_usize);
             let (mut prev, mut aux) = (0, 0);
 
             loop {
-                let cond = (i, j) == (s, p);
-                prev = if cond { matrix[s][p] } else { aux };
+                let cond = (i, j) == (0, p);
+                prev = if cond { matrix[0 + s][p + s] } else { aux };
                 (l, m) = (j, n - 1 - i);
-                aux = matrix[l][m];
-                matrix[l][m] = prev;
+                aux = matrix[l + s][m + s];
+                matrix[l + s][m + s] = prev;
                 (i, j) = (l, m);
 
-                if (i, j) == (s, p) {
+                if (i, j) == (0, p) {
                     break;
                 }
             }
@@ -35,33 +35,46 @@ impl Solution {
     }
 
     pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
-        // Solution::compute(0, 4, matrix);
-        Solution::compute(1, 2, matrix);
-        // Solution::compute(2, 1, matrix);
+        let mut n = matrix[0].len();
+        let mut s = 0_usize;
+
+        loop {
+            if n < 2 {
+                break;
+            }
+
+            Solution::compute(s, n, matrix);
+
+            n = n.saturating_sub(2);
+            s += 1;
+        }
     }
 }
 
 fn main() {
-    let mut matrix_0 = vec![
+    // let mut matrix_0 = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+    // Solution::rotate(&mut matrix_0);
+
+    let mut matrix_1 = vec![
         vec![1, 2, 3, 4],
         vec![5, 6, 7, 8],
         vec![9, 10, 11, 12],
         vec![13, 14, 15, 16],
     ];
-    Solution::rotate(&mut matrix_0);
+    Solution::rotate(&mut matrix_1);
 
     //
-    let mut matrix_1 = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
-    Solution::rotate(&mut matrix_1);
+    let mut matrix_2 = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+    Solution::rotate(&mut matrix_2);
     //
-    let mut matrix_2 = vec![
+    let mut matrix_3 = vec![
         vec![5, 1, 9, 11],
         vec![2, 4, 8, 10],
         vec![13, 3, 6, 7],
         vec![15, 14, 12, 16],
     ];
 
-    Solution::rotate(&mut matrix_2);
+    Solution::rotate(&mut matrix_3);
 }
 
 #[cfg(test)]
