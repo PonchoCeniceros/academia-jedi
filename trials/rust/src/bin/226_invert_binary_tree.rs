7d7bb76cb30558576d5abf0af7addf7db383306f
Giovanny Alfonso Chávez Ceniceros <70242956+PonchoCeniceros@users.noreply.github.com>
2026-07-17 17:18:43 -0600

    [main] problema 226 resuelto | actualizacion a la notas de rust

--- trials/rust/src/bin/226_invert_binary_tree.rs
+++ trials/rust/src/bin/226_invert_binary_tree.rs
@@ -1,5 +1,6 @@
 use colored::*;
 use std::cell::RefCell;
+use std::collections::VecDeque;
 use std::rc::Rc;
 
 struct Solution;
@@ -14,6 +15,11 @@ pub struct TreeNode {
     pub val: i32,
     pub left: Option<Rc<RefCell<TreeNode>>>,
     pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+pub struct TestCase {
+    label: String,
+    value: Node,
 }
 
 /**
@@ -33,34 +39,46 @@ impl Solution {
      * desde atrás de un `&` (préstamo). Hay que abrir el Option prestando, y luego
      * `.borrow()` el RefCell para llegar a los campos.
      */
-    fn pre_order(n: &Node) {
+    pub fn _pre_order(n: &Node, ans: &mut Vec<i32>) {
         if let Some(rc) = n {
             // rc: &Rc<RefCell<TreeNode>> (match ergonomics: liga por referencia)
             let node = rc.borrow(); // nodo: Ref<TreeNode> -> ya puedo leer los campos
-            Solution::pre_order(&node.left); // nodo.left es Node; &nodo.left es &Node
-            Solution::pre_order(&node.right);
-            println!("{}", format!("{:?}", node.val).green().italic().underline()); // visitar(node.val);
+            ans.push(node.val); // visitar(node.val)
+            Solution::_pre_order(&node.left, ans); // nodo.left es Node; &nodo.left es &Node
+            Solution::_pre_order(&node.right, ans);
         }
     }
 
-    fn post_order(n: &Node) {
-        if let Some(rc) = n {
-            // rc: &Rc<RefCell<TreeNode>> (match ergonomics: liga por referencia)
-            let node = rc.borrow(); // nodo: Ref<TreeNode> -> ya puedo leer los campos
-            Solution::post_order(&node.left); // nodo.left es Node; &nodo.left es &Node
-            Solution::post_order(&node.right);
-            println!(
-                "{}",
-                format!("{:?}", node.val).purple().italic().underline()
-            ); // visitar(node.val);
+    pub fn bfs(root: &Node) -> Vec<i32> {
+        let mut ans = Vec::new();
+        let mut queue = VecDeque::new();
+        // Si la raíz existe, la metemos a la queue clonando el Rc
+        if let Some(rc) = root {
+            queue.push_back(Rc::clone(rc));
         }
+        // Mientras queden nodos por procesar en este nivel o los siguientes
+        while let Some(current_rc) = queue.pop_front() {
+            // Accedemos al nodo de forma inmutable
+            let node = current_rc.borrow();
+            // Guardamos el valor en nuestro arreglo de salida
+            ans.push(node.val);
+            // Empujamos los hijos a la queue (si existen)
+            if let Some(ref left) = node.left {
+                queue.push_back(Rc::clone(left));
+            }
+            if let Some(ref right) = node.right {
+                queue.push_back(Rc::clone(right));
+            }
+        }
+
+        ans
     }
 
-    fn invert_tree(n: &Node) {
+    fn _invert_tree(n: &Node) {
         if let Some(rc) = n {
             let mut node = rc.borrow_mut();
-            Solution::invert_tree(&node.left);
-            Solution::invert_tree(&node.right);
+            Solution::_invert_tree(&node.left);
+            Solution::_invert_tree(&node.right);
 
             let tmp_left: Node = node.left.take();
             let tmp_right: Node = node.right.take();
@@ -69,69 +87,31 @@ impl Solution {
         }
     }
 
-    pub fn invert_tree_(root: Node) -> Node {
-        None
+    pub fn invert_tree(root: Node) -> Node {
+        Solution::_invert_tree(&root);
+        root
     }
 }
 
 fn main() {
-    let leaf_20: Node = Some(Rc::new(RefCell::new(TreeNode {
-        val: 1,
-        left: None,
-        right: None,
-    })));
-
-    let leaf_21: Node = Some(Rc::new(RefCell::new(TreeNode {
-        val: 3,
-        left: None,
-        right: None,
-    })));
-
-    let leaf_22: Node = Some(Rc::new(RefCell::new(TreeNode {
-        val: 6,
-        left: None,
-        right: None,
-    })));
-
-    let leaf_23: Node = Some(Rc::new(RefCell::new(TreeNode {
-        val: 9,
-        left: None,
-        right: None,
-    })));
-
-    let leaf_10: Node = Some(Rc::new(RefCell::new(TreeNode {
+    let tree: Node = Some(Rc::new(RefCell::new(TreeNode {
         val: 2,
-        left: leaf_20,
-        right: leaf_21,
+        left: Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: None,
+            right: None,
+        }))),
+        right: Some(Rc::new(RefCell::new(TreeNode {
+            val: 3,
+            left: None,
+            right: None,
+        }))),
     })));
 
-    let leaf_11: Node = Some(Rc::new(RefCell::new(TreeNode {
-        val: 7,
-        left: leaf_22,
-        right: leaf_23,
-    })));
-
-    let tree_0: Node = Some(Rc::new(RefCell::new(TreeNode {
-        val: 4,
-        left: leaf_10,
-        right: leaf_11,
-    })));
-
-    Solution::pre_order(&tree_0);
-    Solution::invert_tree(&tree_0);
-    Solution::post_order(&tree_0);
+    let tree_ans: Node = Solution::invert_tree(tree);
+    let ans: Vec<i32> = Solution::bfs(&tree_ans);
+    println!("{}", format!("{:?}", ans).green().italic().underline());
 }
-
-/*
-// Input: root = [2,1,3]
-// Output: [2,3,1]
-// Input: root = []
-// Output: []
-
-// Input: root = [4,2,7,1,3,6,9]
-// Output: [4,7,2,9,6,3,1]
-// let ans: Node = Solution::invert_tree(tree_0);
-// println!("{}", format!("{:?}", ans).green().italic().underline());
 
 #[cfg(test)]
 mod tests {
@@ -139,19 +119,94 @@ mod tests {
 
     #[test]
     fn test_invert_binary_tree() {
+        //
+        //
+        //
+        let leaf_x: Node = Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: None,
+            right: None,
+        })));
+
+        let leaf_y: Node = Some(Rc::new(RefCell::new(TreeNode {
+            val: 3,
+            left: None,
+            right: None,
+        })));
+
+        let tree_1: Node = Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: leaf_x,
+            right: leaf_y,
+        })));
+
+        //
+        //
+        //
+        let leaf_20: Node = Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: None,
+            right: None,
+        })));
+
+        let leaf_21: Node = Some(Rc::new(RefCell::new(TreeNode {
+            val: 3,
+            left: None,
+            right: None,
+        })));
+
+        let leaf_22: Node = Some(Rc::new(RefCell::new(TreeNode {
+            val: 6,
+            left: None,
+            right: None,
+        })));
+
+        let leaf_23: Node = Some(Rc::new(RefCell::new(TreeNode {
+            val: 9,
+            left: None,
+            right: None,
+        })));
+
+        let leaf_10: Node = Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: leaf_20,
+            right: leaf_21,
+        })));
+
+        let leaf_11: Node = Some(Rc::new(RefCell::new(TreeNode {
+            val: 7,
+            left: leaf_22,
+            right: leaf_23,
+        })));
+
+        let tree_0: Node = Some(Rc::new(RefCell::new(TreeNode {
+            val: 4,
+            left: leaf_10,
+            right: leaf_11,
+        })));
+
         let cases = [
-            // (, ),
+            (
+                TestCase {
+                    label: "tree_0".to_string(),
+                    value: tree_0,
+                },
+                vec![4, 7, 2, 9, 6, 3, 1],
+            ),
+            (
+                TestCase {
+                    label: "tree_1".to_string(),
+                    value: tree_1,
+                },
+                vec![2, 3, 1],
+            ),
         ];
 
         for (input, expected) in cases {
-            todo!();
-            // assert_eq!(
-            //     Solution::invert_binary_tree(),
-            //     expected,
-            //     "{}",
-            //     format!("{:?}", input).red().italic().underline()
-            // );
+            let label = input.label;
+            let tree_ans: Node = Solution::invert_tree(input.value);
+            let ans: Vec<i32> = Solution::bfs(&tree_ans);
+            assert_eq!(ans, expected, "{}", label.red().italic().underline());
         }
     }
 }
-*/
