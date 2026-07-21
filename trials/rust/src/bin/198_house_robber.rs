afd1983357c8c19c23a53ce1cc72fdf1e7e353b4
Giovanny Alfonso Chávez Ceniceros <70242956+PonchoCeniceros@users.noreply.github.com>
2026-07-21 14:06:28 -0600

    [main] problema 198 terminado

--- trials/rust/src/bin/198_house_robber.rs
+++ trials/rust/src/bin/198_house_robber.rs
@@ -8,23 +8,25 @@ struct Solution;
  * Implement your solution here
  */
 impl Solution {
-    // me encuentro en la casa curr (y tengo la adjacencia prev)
+    // me encuentro en la casa curr
     fn solve(curr: usize, houses: &[i32], memo: &mut HashMap<usize, i32>) -> i32 {
         // condicion base: si se termina el arreglo de casa que robar, terminamos
         if curr >= houses.len() {
             return 0;
         }
 
+        /* MEMOIZACION */
         if let Some(&ans) = memo.get(&curr) {
             return ans;
         }
 
-        //
+        // tengo estas 2 decisiones que puedo tomar:
         // () robar la casa actual (y tener que brincarme la sig casa porque no podre robarle)
         // () brincerme la casa actual (y no tomar el botin)
         let steal = houses[curr] + Solution::solve(curr + 2, houses, memo);
         let skip = Solution::solve(curr + 1, houses, memo);
         let ans = max(steal, skip);
+        /* MEMOIZACION */
         memo.insert(curr, ans);
         ans
     }
