53f573dbb0f6dc83f702c8173b8eb749f023a516
Giovanny Alfonso Chávez Ceniceros <70242956+PonchoCeniceros@users.noreply.github.com>
2026-07-28 12:41:04 -0600

    [main] problema 278 solucionado | documentacion de topicos de algoritmos

--- rust/fundamentals/trials/src/bin/278_first_bad_version.rs
+++ rust/fundamentals/trials/src/bin/278_first_bad_version.rs
@@ -4,11 +4,22 @@ struct Solution;
 
 /**
  * Implement your solution here
- *
  */
 impl Solution {
     pub fn first_bad_version(&self, n: i32) -> i32 {
-        0
+        let mut l = 0;
+        let mut r = n;
+
+        while l <= r {
+            let m = l + (r - l) / 2;
+            if self.isBadVersion(m) {
+                r = m - 1
+            } else {
+                l = m + 1
+            }
+        }
+
+        l
     }
 }
 
@@ -25,27 +36,3 @@ fn main() {
     let ans = Solution::first_bad_version(&Solution, 5);
     println!("{}", format!("{}", ans).green().italic().underline());
 }
-
-/*
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_first_bad_version() {
-        let cases = [
-            (5, 4), // bad = 4
-            (1, 1), // bad = 1
-        ];
-
-        for (input, expected) in cases {
-            assert_eq!(
-                Solution::first_bad_version(input),
-                expected,
-                "{}",
-                format!("{:?}", input).red().italic().underline()
-            );
-        }
-    }
-}
-*/
